4e691871a5bea4260cdd4a370a5db85af594d68b
donley828 <344919627@qq.com>
2018-01-22 00:10:00 +0800

    my profile

--- src/page/tab/Profile.vue
+++ src/page/tab/Profile.vue
@@ -7,17 +7,47 @@
       <span class="img-box">
         <img src="../../assets/19424162.jpg" alt="">
       </span>
+      <span class="user">
+        <p>{{ username }}</p>
+        <p><i class="iconfont icon-shouji" style="color:#fff;"></i>{{ hiddenNumber }}</p>
+      </span>
+      <span class="arrow">
+        <i class="iconfont icon-jiantou"></i>
+      </span>
     </div>
+    <group-title>个人信息</group-title>
+    <group>
+      <cell title="注册信息">
+        <i slot="icon" class="iconfont icon-jifen"></i>
+      </cell>
+    </group>
+    <group-title>账户安全</group-title>
+    <group>
+      <cell title="Test">
+        <i slot="icon" class="iconfont icon-jifen"></i>
+      </cell>
+    </group>
   </div>
 </template>
 <script>
-import { XHeader } from 'vux'
+import { XHeader, Group, Cell, GroupTitle } from 'vux'
 export default {
   components: {
-    XHeader
+    XHeader,
+    Group,
+    Cell,
+    GroupTitle
   },
   data() {
-    return {}
+    return {
+      username: 'xxx3210161',
+      phoneNumber: '18730270906'
+    }
+  },
+  computed: {
+    hiddenNumber() {
+      return this.phoneNumber.substr(0, 3) + '****' + this.phoneNumber.substr(7)
+    }
   }
 }
 </script>
@@ -37,11 +67,32 @@ img {
   );
   align-items: center;
   padding: 0 1em;
+  position: relative;
 }
 .img-box {
   display: inline-block;
   width: 5em;
   height: 5em;
+}
+.user {
+  height: 5em;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  justify-content: space-around;
+  padding: 0 1em;
+  color: #fff;
+}
+.arrow {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  position: absolute;
+  right: 1em;
+}
+.arrow i {
+  color: #fff;
+  font-size: 2em;
 }
 </style>
 
